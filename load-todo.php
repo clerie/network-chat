5d9e9b67ae9562f49bc00901cb4065b8d9716541
Clemens <clerie@users.noreply.github.com>
2017-09-21 17:51:13 +0200

    Update load-todo.php

--- load-todo.php
+++ load-todo.php
@@ -1,5 +1,5 @@
 <?php
-$file = "data/todo.txt";
+$file = "data/info.txt";
 $myfile = fopen($file, "r") or die("Unable to open file!");
 echo str_replace("\n", "<br/>", fread($myfile,filesize($file)));
 fclose($myfile);
